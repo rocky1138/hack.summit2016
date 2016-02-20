f29a371065216eabb27f29e2e2369d95ddd1f04c
John Rockefeller <jrockefeller1@gmail.com>
2016-02-20 18:49:32 -0500

    Test level with basic obstacle

--- js/engine.js
+++ js/engine.js
@@ -1,51 +1,56 @@
-var colliders_selector = ".collider";
-var obstacles_selector = ".obstacle";
-var guy_height = $('#guy').css('height');
-var verticalVelocity = 1;
-var horizontalVelocity = 0;
+$(function() {
+    var guy = $('#guy'),
+        colliders_selector = ".collider";
+        obstacles_selector = ".obstacle";
+        guy_height = guy.css('height');
+        verticalVelocity = 1;
+        horizontalVelocity = 0,
+        arrowLeft = 37,
+        arrowUp = 38,
+        arrowRight = 39,
+        arrowDown = 40;
 
-$(document).keydown(function(e) {
-    console.log(e.keyCode);
+    $(document).keydown(function(e) {
+        console.log(e.keyCode);
 
-    if (e.keyCode === 39) {
-        horizontalVelocity = 1;
-    }
-    else if (e.keyCode === 37) {
-        horizontalVelocity = -1;
-    }
-    else if (e.keyCode === 38) {
-        verticalVelocity = 5;
-    }
-});
-
-$(document).keyup(function(e) {
-    horizontalVelocity = 0;
-});
-
-$('#input').change(function() {
-    $('#sandbox').html($('#input').val());
-});
-
-// Gravity 
-setInterval(function() {
-    var hits = $(colliders_selector).collision(obstacles_selector);
-    verticalVelocity = verticalVelocity + (-9.81 * 0.028);
-
-    if (hits.length === 0) {
-        $('#guy').css('top', $('#guy').offset().top + (-1 * verticalVelocity) + 'px');
-    }
-    else {
-        console.log(hits);
-
-        if (verticalVelocity < 0) {
-            $('#guy').css('top', $(hits[0]).offset().top - guy_height);
+        if (e.keyCode === arrowRight) {
+            horizontalVelocity = 1;
+        } else if (e.keyCode === arrowLeft) {
+            horizontalVelocity = -1;
+        } else if (e.keyCode === arrowUp) {
+            verticalVelocity = 5;
         }
-        else {
-            $('#guy').css('top', $('#guy').offset().top + (-1 * verticalVelocity) + 'px');
-        }
-    }
+    });
 
-    if (horizontalVelocity != 0) {
-        $('#guy').css('left', $('#guy').offset().left + (3 * horizontalVelocity) + 'px');
-    }
-}, 28);
+    $(document).keyup(function (e) {
+        horizontalVelocity = 0;
+    });
+
+    $('#input').change(function () {
+        $('#sandbox').html($('#input').val());
+    });
+
+    // Gravity 
+    setInterval(function () {
+        
+        var hits = $(colliders_selector).collision(obstacles_selector);
+        
+        verticalVelocity = verticalVelocity + (-9.81 * 0.028);
+
+        if (hits.length === 0) {
+            guy.css('top', guy.offset().top + (-1 * verticalVelocity) + 'px');
+        } else {
+            console.log(hits);
+
+            if (verticalVelocity < 0) {
+                guy.css('top', $(hits[0]).offset().top - guy_height);
+            } else {
+                guy.css('top', guy.offset().top + (-1 * verticalVelocity) + 'px');
+            }
+        }
+
+        if (horizontalVelocity != 0) {
+            guy.css('left', guy.offset().left + (3 * horizontalVelocity) + 'px');
+        }
+    }, 28);
+});
