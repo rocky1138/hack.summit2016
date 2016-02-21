c573d1749b0036e1ffb6c7428cd4514498b775ef
Fan Zhang <fanwashere@gmail.com>
2016-02-21 05:45:45 -0500

    Added some comments. Not fully explained yet.

--- js/engine.js
+++ js/engine.js
@@ -24,7 +24,7 @@ $(function () {
     $('#level0').css('display', 'block');
         
     // place guy
-    $('.game-window').append('<div class="collider" id="guy" style="background-color: rgba(0, 0, 0, 0);"><div class="collider" id="horizontalCollider" style="height: 15px; width: 16px; background-color: red;"></div></div>');
+    $('.game-window').append('<div class="collider" id="guy" style="top: 100px;background-color: rgba(0, 0, 0, 0);"><div class="collider" id="horizontalCollider" style="height: 15px; width: 16px; background-color: blue;"></div></div>');
     guy = $('#guy');
     guy_height = guy.height();
 
@@ -71,6 +71,8 @@ $(function () {
         }
     });
     
+    // Get collision direction (if any) from both vertical and horizontal colliders. 
+    // Returns string including all directions with collisions.
     function getCollisionDirections() {
         // Get collider data, specifically the direction of collision and the obstacle collided with.
         var horizontalCollider = $('#horizontalCollider.collider').collision(obstacles_selector, { 
@@ -78,22 +80,21 @@ $(function () {
             obstacleData: "odata", 
             directionData: "ddata", 
             as: "<div/>"
-        });
-
-        var verticalCollider = $('#guy.collider').collision(obstacles_selector, { 
+        }),
+        verticalCollider = $('#guy.collider').collision(obstacles_selector, { 
             relative: "#guy", 
             obstacleData: "odata", 
             directionData: "ddata", 
             as: "<div/>"
-        });
+        }),
+        collisions = '';
 
-        var collisions = '';
         // Get all vertical collider collisions.
         for (var i = 0; i < verticalCollider.length; i++) {
             collisions += $(verticalCollider[i]).data('ddata');
         }
 
-        // Remove left and right direction collisions.
+        // Remove left and right direction collisions for vertical collider.
         collisions = collisions.replace("W", "");
         collisions = collisions.replace('E', '');
 
@@ -105,11 +106,16 @@ $(function () {
         return collisions;
     }
 
-    /* PHYSICS SECTION */
+    /* RENDER SECTION */
+    // Frames are rendering using setInterval set to interval equal to the cycle length.
+    // Set the FPS to set how often a frame is rendered.
+    // Rendering is divided into three sections: input constraint, simulation, and update.
     setInterval(function () {
+        // Input Constraint - in this section, check for collisions and use them to constrain the input.
+        // First, get all collision directions.
         var collisions = getCollisionDirections();
 
-        // Jump is allowed if player is standing on some platform (collision at bottom).
+        // Jump is only allowed if player is standing on some platform (collision at bottom).
         if (keyMap.up && collisions.indexOf('S') != -1) {
             verticalVelocity = jumpVelocity;
         }
@@ -127,24 +133,33 @@ $(function () {
             horizontalVelocity = moveVelocity;
         }
         else {
+            // If there are no left or right arrow key press, then no vertical movement.
             horizontalVelocity = 0;
         }
 
-        // Render next frame.
-        // Update vertical location. 
-        // Multipled by -1 because the downwards direction is negative in terms of velocity.
-        // But downwards direction in absolute position is a larger positive number.
-        //guy.css('top', guy.offset().top + (-1 * velocityScaler * verticalVelocity) + 'px');
+        // Simulation - Simulate next frame using current velocities to see if collisions will happen.
+        // Update trajectory collider. The player object uses two seperate colliders: vertical and horizontal.
+        // This is due to the fact that with gravity, the player may eventually accelerate so fast that they can
+        // completed pass through an obstacle in a single frame without every colliding with it.
+        // The vertical collider is implemented such that it represents trajectory.
+        // It extends from the player's current position to its next position (based on velocity).
+        // This way, any obstacles passed in between will be detected in collision detection.
 
-        // Update horizontal location.
-        guy.css('left', guy.offset().left + (velocityScaler * horizontalVelocity) + 'px');
-
+        // Set trajectory collider length (height).
         guy.css('height', guy_height + (-1 * verticalVelocity * velocityScaler) + 'px');
 
-        // Reevaluate collision to check for new collisions in the new frame.
+        // The horiontal position is slightly simpler, since it does not accelerate. It is highly unlikely,
+        // next to impossible, for it to skip over an entire obstacle. Thus, no need for trajectory.
+
+        // Move the player to the next horizontal position.
+        guy.css('left', guy.offset().left + (velocityScaler * horizontalVelocity) + 'px');
+
+        // Reevaluate collision to check for new collisions in the next frame.
         collisions = getCollisionDirections();
 
+        // If there is a bottom collision.
         if (collisions.indexOf('S') != -1) {
+            // Get collided obstacle using vertical collider.
             var collData = $('#guy.collider').collision(obstacles_selector, { 
                 relative: "#guy", 
                 obstacleData: "odata", 
@@ -159,14 +174,18 @@ $(function () {
                 }
             }
 
+            // No more downwards velocity.
             verticalVelocity = 0;
 
-            guy.css('top', guy.offset().top + bottomLimit - 15 + 'px');
+            // Set player position.
+            guy.css('top', guy.offset().top + bottomLimit - (guy_height - 1) + 'px');
         }
         else {
+            // If there is no bottom collision, just set player position based on velocity.
             guy.css('top', guy.offset().top + (-1 * velocityScaler * verticalVelocity) + 'px');
         }
 
+        // For left and right collisions.
         if (collisions.indexOf('W') != -1) {
             var collData = $('#horizontalCollider.collider').collision(obstacles_selector, { 
                 relative: "#horizontalCollider", 
@@ -175,16 +194,16 @@ $(function () {
                 as: "<div/>"
             });
 
-            if (collData[0]) {
+      
                 var leftLimit = parseFloat($(collData[0]).css('width').replace('px', ''));
                 for (var i = 1; i < collData.length; i++) {
                     if (parseFloat($(collData[i]).css('width').replace('px', '')) < leftLimit) {
                         leftLimit = parseFloat($(collData[i]).css('width').replace('px', ''));
                     }
                 }
-            }
+
+                guy.css('left', guy.offset().left + leftLimit + 'px');
             
-            guy.css('left', guy.offset().left + leftLimit + 'px');
         }
         else if (collisions.indexOf('E') != -1) {
             var collData = $('#horizontalCollider.collider').collision(obstacles_selector, { 
