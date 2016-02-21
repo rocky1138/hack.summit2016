11d5d9e939f85ed2e002f517bb647a0ee2462aad
Richard Bettridge <r.bettridge@vehikl.com>
2016-02-21 01:47:25 -0500

    dem levels

--- js/game.js
+++ js/game.js
@@ -1,20 +1,20 @@
 // Functionality related to game logic, e.g., loading levels, points.
 $(function () {
-    
+
     'use strict';
-    
+
     var links = $('link[rel="import"]');
-    
+
     // When stats object changes, run callback.
     watch (stats, function () {
-        
+
         var level = links[stats.level].import;
-        
+
         // Select and display our level.
         $('.game-window').html(level.querySelector('#level' + stats.level).innerHTML);
     });
-    
-    stats.level = 0;
+
+    stats.level = 4;
 });
 
 $('.game-window').append('<div style="position:absolute;width:400px;bottom:0;height:100px;"></div>');
