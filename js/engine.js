c6559cac8c59e7a30669cc40e3e69f7cb81802ee
Richard Bettridge <r.bettridge@vehikl.com>
2016-02-20 20:56:33 -0500

    jump mostly works

--- js/engine.js
+++ js/engine.js
@@ -1,9 +1,10 @@
 $(function() {
     var guy = $('#guy'),
         colliders_selector = ".collider";
-        obstacles_selector = ".obstacle";
+        obstacles_selector = ".obstacle",
         guy_height = guy.css('height');
-        verticalVelocity = 1;
+        lastColDir = '',
+        verticalVelocity = 1,
         horizontalVelocity = 0,
         arrowLeft = 37,
         arrowUp = 38,
@@ -17,7 +18,8 @@ $(function() {
             horizontalVelocity = 1;
         } else if (e.keyCode === arrowLeft) {
             horizontalVelocity = -1;
-        } else if (e.keyCode === arrowUp && verticalVelocity < 0 ) {
+        } else if (e.keyCode === arrowUp && lastColDir == "S" ) {
+            lastColDir = '';
             verticalVelocity = 5;
         }
     });
@@ -33,19 +35,23 @@ $(function() {
     // Gravity
     setInterval(function () {
 
-        var hits = $(colliders_selector).collision(obstacles_selector);
+        var collData = $(colliders_selector).collision(obstacles_selector, { relative: "collider", obstacleData: "odata", colliderData: "cdata", directionData: "ddata", as: "<div/>"} );
+        var hits = $(colliders_selector).collision(obstacles_selector); //hacky above, blame richard
 
         verticalVelocity = verticalVelocity + (-9.81 * 0.028);
         if (hits.length === 0) {
             guy.css('top', guy.offset().top + (-1 * verticalVelocity) + 'px');
         } else {
+            console.dir(hits[0].id);
+            console.dir($(collData[0]).data("ddata"));
+            lastColDir = $(collData[0]).data("ddata"); //for jumping
+
             if  (hits[0].id ==  "deathfield" ){
                 verticalVelocity = 0;
                 guy.css('top', 0);
                 guy.css('left', 0);
                 return;
             }
-            console.log(hits);
 
             if (verticalVelocity < 0) {
                 guy.css('top', $(hits[0]).offset().top - guy_height);
