6f3fbba0ffb465fece3f3f98fbabaf7b6d011d81
Muhammad Muhaimin <muhammad.r.muhaimin@gmail.com>
2016-02-21 19:06:03 -0500

    Mouse tooltip

--- js/input.js
+++ js/input.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
   'use strict';
-  var hrefValue = "";
+  var hrefValue = "",
+      currentMousePos = { x: -1, y: -1},
+      windowHeight =  $( window ).height();
+
   $("#update").click(function() {
     document.getElementById('pickup').play();
     var text = $('textarea#htmlcommand').val();
@@ -16,7 +19,6 @@ $(document).ready(function () {
   /* Code for toggle Music */
   $('#music-button').click(function(){
     var volume = $('#music').attr('volume');
-    console.log(volume);
     if(volume === "1"){
       $('#music').prop('volume', 0);
       $('#music').attr('volume', 0);
@@ -25,5 +27,13 @@ $(document).ready(function () {
       $('#music').prop('volume', 1);
       $('#music').attr('volume', 1);
     }
+  });
+
+  /* Code for mouse position tooltip */
+  $(document).mousemove(function(event) {
+    currentMousePos.x = event.pageX;
+    currentMousePos.y = event.pageY;
+    $('.tooltip').css('left', event.pageX + 10).css('top', event.pageY + 10).css('display', 'block');
+    $('.tooltip').html( "( " + currentMousePos.x + ", " + (windowHeight - currentMousePos.y) + " )" );
   });
 });
